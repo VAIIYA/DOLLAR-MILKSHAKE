bc92b34d092bdaf4e5034e781fe5aa5660e3c57d
MG <53813521+meneergroot@users.noreply.github.com>
2026-03-08 10:54:05 +0100

    Remove test-noon.ts to fix build failure

--- test-noon.ts
+++
@@ -1,4 +0,0 @@
-import { getNextNoonUTC } from "./src/lib/fees";
-
-console.log("Current time:", new Date().toISOString());
-console.log("Calculated Next Noon UTC:", getNextNoonUTC().toISOString());
